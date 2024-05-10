a4ad9bc6cec81d5dbd23992f14be9668c3a92987
komal-rndtechnosoft <komal.babar@rndtechnosoft.in>
2024-05-10 11:44:48 +0530

    index page setup

---
+++ resources/views/Frontend/layout/head.blade.php
@@ -0,0 +1,13 @@
+
+	<!-- CSS here -->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="assets/fonts/bootstrap-icons/font-css.css" />
+	<link rel="stylesheet" href="assets/css/animate.min.css" />
+	<link rel="stylesheet" href="assets/css/swiper-bundle.min.css" />
+	<link rel="stylesheet" href="assets/css/all.min.css" />
+	<link rel="stylesheet" href="assets/css/nice-select.css" />
+	<link rel="stylesheet" href="assets/css/magnific-popup.css" />
+	<link rel="stylesheet" href="assets/css/metisMenu.css" />
+	<link rel="stylesheet" href="assets/css/aos.css" />
+	<link rel="stylesheet" href="assets/css/spacing.css" />
+	<link rel="stylesheet" href="assets/css/main.css" />
---
+++ resources/views/Frontend/layout/nav.blade.php
@@ -0,0 +1,282 @@
+<!-- preloader -->
+
+
+
+<div id="preloader">
+			<div class="preloader">
+				<span></span>
+				<span></span>
+			</div>
+		</div>
+		<!-- preloader end  -->
+		<!-- offcanvas start  -->
+		<div class="offcanvas offcanvas-top heding-bg" tabindex="-1" id="offcanvasTop">
+			<div class="offcanvas-header">
+				<a data-bs-dismiss="offcanvas" aria-label="Close">
+					<i class="fas fa-times search-close" id="search-close"></i>
+				</a>
+			</div>
+			<div class="offcanvas-body">
+				<!-- Fullscreen search -->
+				<div class="search-wrap">
+					<form method="get">
+						<input type="search" class="main-search-input" placeholder="Search Your Keyword...">
+					</form>
+				</div>
+				<!-- end fullscreen search -->
+			</div>
+		</div>
+		<!-- offcanvas end  -->
+
+		<!-- header-area start -->
+		<header class="theme-main-menu theme-menu-three">
+			<div class="container header-custom-container pt-20 pb-20">
+				<div class="row">
+					<div class="col-lg-3 d-none d-lg-inline-block">
+						<div class="logo-area">
+							<a class="front" href="index.html"><img src="assets/img/logo/header-logo-solar.png"
+									alt="Header-logo"></a>
+						</div>
+					</div>
+					<div class="col-lg-9 col-12">
+						<ul
+							class="list-none header-info d-flex align-items-center justify-content-center justify-content-lg-end">
+							<li>
+								<div class="header-info-box">
+									<div class="icon">
+										<img src="assets/img/icon/icon-35c.svg" alt="Phone">
+									</div>
+									<div class="text-content">
+										<span>Call</span>
+										<h6>(505) 555-0125</h6>
+									</div>
+								</div>
+							</li>
+							<li class="d-none d-md-inline-block">
+								<div class="header-info-box">
+									<div class="icon">
+										<img src="assets/img/icon/icon-36c.svg" alt="Phone">
+									</div>
+									<div class="text-content">
+										<span>Location</span>
+										<div class="right-language">
+											<div class="dropdown">
+												<a class="language-btn dropdown-toggle" href="#" role="button"
+													id="dropdownMenuLink" data-bs-toggle="dropdown"
+													aria-expanded="false">
+													USA, New York
+												</a>
+												<ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+													<li>
+														<a class="dropdown-item" href="#">Washington DC</a>
+													</li>
+													<li>
+														<a class="dropdown-item" href="#">Nevrada</a>
+													</li>
+													<li>
+														<a class="dropdown-item" href="#">New Walsh</a>
+													</li>
+													<li>
+														<a class="dropdown-item" href="#">Merily Land</a>
+													</li>
+													<li>
+														<a class="dropdown-item" href="#">Hampshire</a>
+													</li>
+												</ul>
+											</div>
+										</div>
+									</div>
+								</div>
+							</li>
+							<li class="d-none d-lg-inline-block">
+								<div class="header-info-box">
+									<div class="icon">
+										<img src="assets/img/icon/icon-37c.svg" alt="Phone">
+									</div>
+									<div class="text-content">
+										<span>Time</span>
+										<h6>10AM - 11:30PM</h6>
+									</div>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="main-header-area pt-15 pb-10">
+				<div class="container header-custom-container">
+					<div class="row">
+						<div class="col-lg-10 col-6">
+							<div class="logo-area d-lg-none d-md-inline-block">
+								<a class="front" href="index.html"><img src="assets/img/logo/header-logo-solar.png"
+										alt="Header-logo"></a>
+							</div>
+							<div class="main-menu d-none d-lg-block">
+								<nav id="mobile-menu">
+									<ul class="menu-list ps-0">
+										<li>
+											<a href="#">
+												Home
+											</a>
+											<ul class="sub-menu">
+												<li>
+													<a href="index.html">Home Style 1</a>
+												</li>
+												<li>
+													<a href="index-2.html">Home Style 2</a>
+												</li>
+												<li>
+													<a href="index-3.html">Home Style 3</a>
+												</li>
+											</ul>
+										</li>
+										<li>
+											<a href="#">
+												Pages
+											</a>
+											<ul class="sub-menu">
+												<li>
+													<a href="about.html">About Us</a>
+												</li>
+												<li>
+													<a href="#">Services <span class="sub-menu-arrow"><i
+																class="bi bi-chevron-down"></i></span></a>
+													<ul class="sub-menu">
+														<li>
+															<a href="services.html">Services</a>
+														</li>
+														<li>
+															<a href="services-details.html">Services Details</a>
+														</li>
+													</ul>
+												</li>
+												<li>
+													<a href="#">Shop <span class="sub-menu-arrow"><i
+																class="bi bi-chevron-down"></i></span></a>
+													<ul class="sub-menu">
+														<li>
+															<a href="product.html">Product</a>
+														</li>
+														<li>
+															<a href="product-details.html">Product Details</a>
+														</li>
+													</ul>
+												</li>
+												<li>
+													<a href="team.html">Team</a>
+												</li>
+												<li>
+													<a href="testimonial.html">Testimonial</a>
+												</li>
+												<li>
+													<a href="cart.html">Cart</a>
+												</li>
+												<li>
+													<a href="checkout.html">Checkout</a>
+												</li>
+											</ul>
+										</li>
+										<li>
+											<a href="#">Project</a>
+											<ul class="sub-menu">
+												<li>
+													<a href="project-grid.html">Project</a>
+												</li>
+												<li>
+													<a href="project-details.html">Project
+														Details</a>
+												</li>
+											</ul>
+										</li>
+										<li>
+											<a href="#">Blog</a>
+											<ul class="sub-menu">
+												<li>
+													<a href="blog-grid.html">Blog Grid</a>
+												</li>
+												<li>
+													<a href="blog-classic.html">Blog
+														Classic</a>
+												</li>
+												<li>
+													<a href="blog-details.html">Blog Details</a>
+												</li>
+											</ul>
+										</li>
+										<li>
+											<a href="contact.html">contact</a>
+										</li>
+									</ul>
+								</nav>
+							</div>
+						</div>
+						<div class="col-lg-2 col-6">
+							<div class="right-nav mb-0 d-flex align-items-center justify-content-end ms-5">
+								<div class="search-area me-4">
+									<a class="search_input" href="#" data-bs-toggle="offcanvas"
+										data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">
+										<img src="assets/img/icon/search.svg" alt="Search">
+									</a>
+								</div>
+								<div class="hamburger-menu">
+									<a class="round-menu" href="javascript:void(0);">
+										<i class="far fa-bars"></i>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- /.theme-main-menu -->
+		</header>
+		<!-- header-area end -->
+
+		<!-- slide-bar start -->
+		<aside class="slide-bar">
+			<div class="close-mobile-menu">
+				<a href="javascript:void(0);">
+					<i class="fas fa-times"></i>
+				</a>
+			</div>
+			<!-- offset-sidebar start -->
+			<div class="offset-sidebar">
+				<div class="offset-widget offset-logo mb-30">
+					<a href="index.html">
+						<img src="assets/img/logo/header-logo-solar.png" alt="logo">
+					</a>
+				</div>
+				<div class="mobile-menu"></div>
+				<div class="offset-widget mb-40">
+					<div class="info-widget">
+						<h4 class="offset-title mb-20">About Us</h4>
+						<p class="mb-30">
+							But I must explain to you how all this mistaken idea of denouncing pleasure and
+							praising pain was born and will give you a complete account of the system and
+							expound the actual teachings of the great explore
+						</p>
+					</div>
+				</div>
+				<div class="offset-widget mb-30 pr-10">
+					<div class="info-widget info-widget2">
+						<h4 class="offset-title mb-20">Contact Info</h4>
+						<p>
+							<i class="fal fa-address-book"></i>
+							23/A, Miranda City Likaoli Prikano, Dope</p>
+						<p>
+							<i class="fal fa-phone"></i>
+							+0989 7876 9865 9
+						</p>
+						<p>
+							<i class="fal fa-envelope-open"></i>
+							info@example.com
+						</p>
+					</div>
+				</div>
+				<!-- <div class="login-btn text-center">
+					<a class="ht_btn w-100" href="login.html">Login</a>
+				</div> -->
+			</div>
+			<!-- offset-sidebar end -->
+
+		</aside>
